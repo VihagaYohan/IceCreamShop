c6f49ee9b48add9c0c62aa3aada69b70012b8135
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-01-21 00:00:49 +0530

    Removed old code

--- build/src-backup/helpers/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.printConsole = void 0;
-// print console.log
-const printConsole = (context) => {
-    console.log(context);
-};
-exports.printConsole = printConsole;
